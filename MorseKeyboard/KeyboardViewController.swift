efa045238caf1b3653dc98be2a6df462391f3435
Dang Quoc Huy <huysamdua@yahoo.com>
2018-07-31 12:01:33 +0700

    Customizing Keyboard UI

--- MorseKeyboard/KeyboardViewController.swift
+++ MorseKeyboard/KeyboardViewController.swift
@@ -30,52 +30,25 @@ import UIKit
 
 class KeyboardViewController: UIInputViewController {
   
-  @IBOutlet var nextKeyboardButton: UIButton!
-  
-  override func updateViewConstraints() {
-    super.updateViewConstraints()
-    
-    // Add custom view sizing constraints here
-  }
+  var morseKeyboardView: MorseKeyboardView!
   
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    // Perform custom UI setup here
-    self.nextKeyboardButton = UIButton(type: .system)
+    let nib = UINib(nibName: "MorseKeyboardView", bundle: nil)
+    let objects = nib.instantiate(withOwner: nil, options: nil)
+    morseKeyboardView = objects.first as! MorseKeyboardView
+    guard let inputView = inputView else { return }
+    inputView.addSubview(morseKeyboardView)
     
-    self.nextKeyboardButton.setTitle(NSLocalizedString("Next Keyboard", comment: "Title for 'Next Keyboard' button"), for: [])
-    self.nextKeyboardButton.sizeToFit()
-    self.nextKeyboardButton.translatesAutoresizingMaskIntoConstraints = false
+    morseKeyboardView.translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate([
+      morseKeyboardView.leftAnchor.constraint(equalTo: inputView.leftAnchor),
+      morseKeyboardView.topAnchor.constraint(equalTo: inputView.topAnchor),
+      morseKeyboardView.rightAnchor.constraint(equalTo: inputView.rightAnchor),
+      morseKeyboardView.bottomAnchor.constraint(equalTo: inputView.bottomAnchor)
+      ])
     
-    self.nextKeyboardButton.addTarget(self, action: #selector(handleInputModeList(from:with:)), for: .allTouchEvents)
-    
-    self.view.addSubview(self.nextKeyboardButton)
-    
-    self.nextKeyboardButton.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
-    self.nextKeyboardButton.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
+    morseKeyboardView.setNextKeyboardVisible(needsInputModeSwitchKey)
   }
-  
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated
-  }
-  
-  override func textWillChange(_ textInput: UITextInput?) {
-    // The app is about to change the document's contents. Perform any preparation here.
-  }
-  
-  override func textDidChange(_ textInput: UITextInput?) {
-    // The app has just changed the document's contents, the document context has been updated.
-    
-    var textColor: UIColor
-    let proxy = self.textDocumentProxy
-    if proxy.keyboardAppearance == UIKeyboardAppearance.dark {
-      textColor = UIColor.white
-    } else {
-      textColor = UIColor.black
-    }
-    self.nextKeyboardButton.setTitleColor(textColor, for: [])
-  }
-  
 }
